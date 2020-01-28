20181cbc913d282e33f0d018ace900d7a66f7161
Robson Lima <rsl.robson@gmail.com>
2020-01-28 09:59:36 -0300

    Solução Alura para o desafio

--- ogros.c
+++ ogros.c
@@ -2,36 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
+//https://github.com/alura-cursos/maratonaprogramacao/blob/aula4-exc/ogros.cpp
 int buscaBin (int ini, int end, int val, int v[]) {
 	if (ini == end) return ini;
-	
-	/*if (ini < end) {
-		if (v[ini] == val) return ini;
-		else return -1;	
-	}*/
 	
 	int meio = (ini + end) / 2;
 	
 	if (v[meio] <= val) return buscaBin(meio + 1, end, val, v);
-	//else if (v[meio] == val) return buscaBin(ini, meio, val, v);
-	//else return buscaBin(ini, meio - 1, val, v);
 	else return buscaBin(ini, meio, val, v);
 }
 
 int main () {
 	
 	FILE *fp;
-	fp = freopen("input2.txt", "r", stdin);
+	fp = freopen("input1.txt", "r", stdin);
 	
 	if (fp != NULL) {
 		int qtd_faixas = 0;
 		int qtd_ogros = 0;
 		
-		int buf[12];
-		fgets(buf, 12, fp);
-		printf("%s\n", buf);
+		char buf[10009];
+		fgets(buf, 10009, fp);
 		sscanf(buf, "%d %d", &qtd_faixas, &qtd_ogros);
-		printf("qtd faixas %d - qtd ogros %d\n", qtd_faixas, qtd_ogros);
 		
 		int i = 0;
 		int faixas[qtd_faixas - 1];
@@ -39,44 +31,33 @@ int main () {
 		int forca_ogro[qtd_ogros];
 		char *pEnd;
 		
-		fgets(buf, 12, fp);
-		printf("%s\n", buf);
+		fgets(buf, 10009, fp);
 		for (i = 0; i < qtd_faixas - 1; i++) {
 			faixas[i] = strtol(buf, &pEnd, 10);
 			memcpy(buf, &pEnd[1], strlen(pEnd));
-			printf("%d ", faixas[i]);
 		}
-		printf("\n");
+		
+		faixas[qtd_faixas - 1] = 2000000000;
 		
 		pEnd = NULL;
-		fgets(buf, 12, fp);
-		printf("%s\n", buf);
+		fgets(buf, 10009, fp);
 		for (i = 0; i < qtd_faixas; i++) {
 			premios[i] = strtol(buf, &pEnd, 10);
-			memcpy(buf, &pEnd[1], strlen(pEnd));	
-			printf("%d ", premios[i]);		
+			memcpy(buf, &pEnd[1], strlen(pEnd));
 		}
-		printf("\n");
 	
 		pEnd = NULL;
-		fgets(buf, 12, fp);
-		printf("-%s\n", buf);
+		fgets(buf, 10009, fp);
 		for (i = 0; i < qtd_ogros; i++) {
 			forca_ogro[i] = strtol(buf, &pEnd, 10);
-			memcpy(buf, &pEnd[1], strlen(pEnd));
-			printf("%d ", forca_ogro[i]);				
+			memcpy(buf, &pEnd[1], strlen(pEnd));		
 		}
-		printf("\n");
 		
 		for (i = 0; i < qtd_ogros; i++) {
-			int premio_ogro = buscaBin(0, qtd_ogros, forca_ogro[i], faixas);
+			int premio_ogro = buscaBin(0, qtd_faixas - 1, forca_ogro[i], faixas);
 			printf("%d ", premios[premio_ogro]);
-		}
-		
+		}		
 		printf("\n");
-		
-		//int rog1 = buscaBin(0, qtd_ogros, forca_ogro[0], faixas);
-		//printf("premio %d\n", premios[rog1]);
 	}
 	
 	return (0);
